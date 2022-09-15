cc9b8d27c8cde4f0592584747f5a6e0786d45278
Matthew Hurst <matt@workpail.com>
2022-09-15 10:57:28 -0400

    Add comments to BottomSheetBarListener

--- lib/bottom_sheet_bar_listener.dart
+++ lib/bottom_sheet_bar_listener.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
+/// Creates a [Listener] to track swipe gestures used to expand or collapse
+/// a [BottomSheetBar]
+///
+/// - Tracks scolling and vertical-pan gestures
+/// - Can be [locked] to stop listening to gestures
+///
+/// There's nothing specific to [BottomSheetBar] in here. This widget just
+/// exposes a version of a [Listener] widget that is easier to use and read.
 class BottomSheetBarListener extends StatelessWidget {
-  final bool locked;
   final Widget child;
+
+  /// Disables bottom-sheet from being expanded or collapsed with a swipe
+  final bool locked;
+
+  /// Triggered on [Listener.onPointerMove] and [Listener.onPointerSignal] (when
+  ///  the [PointerSignalEvent] is a [PointerScrollEvent])
   final void Function(double dy) onScroll;
+
+  /// Triggered on [Listener.onPointerDown] and [Listener.onPointerMove]
   final void Function(Duration timestamp, Offset position) onPosition;
+
+  /// Trigger on [Listener.onPointerUp] and [Listener.onPointerCancel]
   final void Function() onEnd;
 
   const BottomSheetBarListener({
